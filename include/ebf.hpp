69c768fe57e83e7ada92201dde34ba39c500b479
Ikytsu <155775453+Ikytsu@users.noreply.github.com>
2024-06-21 18:15:16 +0200

    an header including many infos to be easy to edit!

---
+++ include/ebf.hpp
@@ -0,0 +1,38 @@
+#ifndef EBF_H
+#define EBF_H
+
+#include <string>
+#include <fstream>
+#include <streambuf>
+#include <iostream>
+#include <filesystem>
+#include <regex>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <stdexcept>
+#include <sstream>
+
+//General output
+static std::string help_output = "-file [filename] : specify a file for compilation"
+    	"\n-debug : indicate all steps of the compilation"
+    	"\n-version : indicate version of the compiler";
+static std::string version_output = "work in progress :)";
+static std::string debug1_output = "starting compilation of";
+static std::string debug2_output = "enter in the code optimization";
+static std::string debug3_output = "no indicated file, terminating the process";
+
+
+//Warnings
+static std::string multiplearg_warning = "Warning: an argument has been put multiple times, ignoring it and the next if it's -file";
+static std::string unknownarg_warning = "Warning: an unknown arg has been put, ignoring it"; 
+
+//Errors
+static std::string filenullptr_error = "Error: -file at the end of the args";
+static std::string unexistingfile_error = "Error: The file doesn't exists";
+static std::string incorrectextenstion_error = "Error: Incorrect extension, the correct ones are .bf, .ebf, .b and .nbf";
+
+//Functions
+
+
+#endif
