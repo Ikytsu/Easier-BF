13a4fd4979fc43a3cb4cc388c31f7bcab06457c3
Ikytsu <155775453+Ikytsu@users.noreply.github.com>
2024-06-23 21:40:07 +0200

    started to write generate_c_code function

--- src/generate-c-code.cpp
+++ src/generate-c-code.cpp
@@ -2,5 +2,21 @@
 
 void generate_c_code(std::string code, std::vector<std::string> config, bool debug)
 {
-	
+	int openloop_count = 0;
+	int closeloop_count = 0;
+	for(char character : code)
+	{
+		if(character == '[')
+		{
+			openloop_count++;
+		}
+		else if(character == ']')
+		{
+			closeloop_count++:
+		}
+	}
+	if(openloop_count != closeloop_count)
+	{
+		std::cout << noclosedloop_error << std::endl;
+	}
 }
